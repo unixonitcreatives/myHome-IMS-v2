16eefbcbd29dd0ddc77fa5eb22372d0f309f66e3
Joff <joffpascual@gmail.com>
2019-11-12 00:13:30 +0800

    Update config.php
    jhg

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '37.59.55.185');//37.59.55.185
-define('DB_USERNAME', 'xAcmiyzWOF'); //xAcmiyzWOF
-define('DB_PASSWORD', 'cQZ2tArDGK'); //cQZ2tArDGK
-define('DB_NAME', 'xAcmiyzWOF');
+define('DB_SERVER', 'unixondev.com');
+define('DB_USERNAME', 'vipfouuo_myhome');
+define('DB_PASSWORD', 'T@Wj+rt_JJlN');
+define('DB_NAME', 'vipfouuo_myhomev2');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Something Went Wrong" . mysqli_connect_error());
