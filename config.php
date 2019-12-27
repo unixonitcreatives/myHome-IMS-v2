d0f2e6ec736e283ffb5f0269a0dcc9727b2ccd6d
Joff <joffpascual@gmail.com>
2019-12-27 11:53:24 +0800

    dasdasd
    asdasdas

--- config.php
+++ config.php
@@ -1,10 +1,15 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');//unixondev.com
-define('DB_USERNAME', 'root');//vipfouuo_myhome
-define('DB_PASSWORD', '');//T@Wj+rt_JJlN
-define('DB_NAME', 'xacmiyzwof');//vipfouuo_myhomev2
+/*define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'xacmiyzwof');*/
+
+define('DB_SERVER', 'unixondev.com');//unixondev.com
+define('DB_USERNAME', 'vipfouuo_myhome');//vipfouuo_myhome
+define('DB_PASSWORD', 'T@Wj+rt_JJlN');//T@Wj+rt_JJlN
+define('DB_NAME', 'vipfouuo_myhomev2');//vipfouuo_myhomev2
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Something Went Wrong" . mysqli_connect_error());
